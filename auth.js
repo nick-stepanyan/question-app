c40c2247758020e5effdc0890648080af7b67800
nick <nick.win.32@mail.ru>
2023-03-23 15:17:57 +0300

    experiment

---
+++ auth.js
@@ -0,0 +1,39 @@
+export function getAuthForm() {
+    return `
+      <form class="mui-form" id="auth-form">
+        <div class="mui-textfield mui-textfield--float-label">
+          <input type="email" id="email" required>
+          <label for="email">Email</label>
+        </div>
+        <div class="mui-textfield mui-textfield--float-label">
+          <input type="password" id="password" required>
+          <label for="password">Пароль</label>
+        </div>
+        <button
+          type="submit"
+          class="mui-btn mui-btn--raised mui-btn--primary"
+        >
+          Войти
+        </button>
+      </form>
+    `
+}
+
+
+export function authWithEmailAndPassword(email, password) {
+    const apiKey = 'AIzaSyBAVpDkJyZg7eBGprdSQqLdDuGBcqy6xrU'
+    return fetch(`https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=${apiKey}`, {
+            method: 'POST',
+            body: JSON.stringify({
+                email,
+                password,
+                returnSecureToken: true
+            }),
+            headers: {
+
+                'Content-type': 'application/json'
+            }
+        })
+        .then(responce => responce.json())
+        .then(data => data.idToken)
+}
